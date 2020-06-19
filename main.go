7f4925a7867883b703637d3eb1801acbfe8dfee6
Rob Garth <robgarth@pingidentity.com>
2020-06-19 23:28:15 +1000

    Adding support for pulling personal stats, and a help menu

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 
-	"github.com/nlopes/slack"
+	"github.com/slack-go/slack"
 	"github.com/tkanos/gonfig"
 )
 
@@ -24,6 +26,11 @@ type configuration struct {
 	DBUser     string
 	DBPass     string
 	SlackToken string
+}
+
+type userStats struct {
+	sent     int
+	received int
 }
 
 func getenv(name string) string {
@@ -85,8 +92,47 @@ func storeKudos(configuration *configuration, sender string, recipients []string
 		return err
 	}
 	defer insert.Close()
-	fmt.Println("Kudos saved in db")
+	log.Println("Kudos saved in db")
 	return nil
+}
+
+func getStats(configuration *configuration, user string) (userStats, error) {
+	var stats userStats
+	dbConnectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		configuration.DBUser,
+		configuration.DBPass,
+		configuration.DBHost,
+		configuration.DBPort,
+		configuration.DBName)
+	db, err := sql.Open("mysql", dbConnectionString)
+	if err != nil {
+		return stats, err
+	}
+	defer db.Close()
+	sent := fmt.Sprintf("select COUNT(sender) FROM kudos_log WHERE timestamp >= LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 MONTH   AND timestamp < LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY AND TRIM(sender) = \"%s\"", user)
+	rcvd := fmt.Sprintf("select COUNT(recipient) FROM kudos_log WHERE timestamp >= LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 MONTH   AND timestamp < LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY AND TRIM(recipient) = \"%s\"", user)
+	sentQuery, err := db.Query(sent)
+	defer sentQuery.Close()
+	if err != nil {
+		return stats, err
+	}
+	for sentQuery.Next() {
+		err := sentQuery.Scan(&stats.sent)
+		if err != nil {
+			return stats, err
+		}
+	}
+	rcvQuery, err := db.Query(rcvd)
+	if err != nil {
+		return stats, err
+	}
+	for rcvQuery.Next() {
+		err := rcvQuery.Scan(&stats.received)
+		if err != nil {
+			return stats, err
+		}
+	}
+	return stats, nil
 }
 
 func main() {
@@ -102,7 +148,7 @@ func main() {
 	}
 
 	configuration.SlackToken = getenv("SLACKTOKEN")
-	configuration.DBPass = getenv("MYSQL_PASS")
+	configuration.DBPass = getenv("MYSQLPASS")
 	api := slack.New(configuration.SlackToken)
 	rtm := api.NewRTM()
 
@@ -120,19 +166,22 @@ Loop:
 				text := ev.Text
 				text = strings.TrimSpace(text)
 				text = strings.ToLower(text)
+				sender, _ := rtm.GetUserInfo(ev.User)
 
 				carrotsMatch := regexp.MustCompile(fmt.Sprintf(":%s:", configuration.Emoji))
 				usersMatch := regexp.MustCompile("@([A-Za-z]+[A-Za-z0-9-_]+)")
+				cmdMatch := regexp.MustCompile(fmt.Sprintf(`^<@%s> (.+)$`, strings.ToLower(info.User.ID)))
+
 				carrots := carrotsMatch.FindAllStringIndex(text, -1)
 				recipients := usersMatch.FindAllString(text, -1)
+				atCmd := cmdMatch.FindStringSubmatch(text)
 
 				if ev.User != info.User.ID && len(carrots) > 0 && len(recipients) > 0 {
-					sender, _ := rtm.GetUserInfo(ev.User)
 					// verify recipients are valid
 					verified, err := verifyRecipients(&configuration, rtm, ev, recipients)
 
 					if err != nil {
-						rtm.SendMessage(rtm.NewOutgoingMessage(err.Error(), ev.Channel))
+						rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprintf("@%s %s", info.User.ID, err.Error()), ev.Channel))
 					} else {
 						//Genuine Kudos!
 						//save to db
@@ -143,7 +192,7 @@ Loop:
 
 						// Acknowledge the carrots
 						var botResp string
-						fmt.Printf("%s sent %d %s to %d verified users\n", sender.Profile.RealName, len(carrots), configuration.Plural, len(verified))
+						log.Printf("%s sent %d %s to %d verified users\n", sender.Profile.RealName, len(carrots), configuration.Plural, len(verified))
 						for i := 0; i < (len(carrots) * len(verified)); i++ {
 							botResp = fmt.Sprintf("%s :%s:", botResp, configuration.Emoji)
 						}
@@ -152,12 +201,46 @@ Loop:
 					}
 
 				}
+				if atCmd != nil {
+					switch atCmd[len(atCmd)-1] {
+					case "me":
+						stats, err := getStats(&configuration, sender.ID)
+						var respStr string
+						if err != nil {
+							log.Println(err)
+							respStr = fmt.Sprintf("Sorry, I encountered a problem and couldn't look up your stats")
+						} else {
+							_, monthStr, _ := time.Now().Date()
+							respStr = fmt.Sprintf("Hey, so far in %s, you have given *%d* %s, and received *%d*", monthStr, stats.sent, configuration.Plural, stats.received)
+						}
+						text := slack.MsgOptionText(respStr, false)
+						user := slack.MsgOptionAsUser(true)
+						rtm.PostEphemeral(ev.Channel, sender.ID, text, user)
+
+					case "help":
+						user := slack.MsgOptionAsUser(true)
+						text := slack.MsgOptionText(
+							fmt.Sprintf("Send :%s: to your peers:", configuration.Emoji)+
+								fmt.Sprintf("\n>Hey @ringo have some :%s: :%s: for your hard work",
+									configuration.Emoji, configuration.Emoji)+
+								fmt.Sprintf("\n>:%s: @paul @john", configuration.Emoji)+
+								fmt.Sprintf("\n>Thanks for you help today @george, have a :%s:", configuration.Emoji)+
+								fmt.Sprintf("\nOther stuff:")+
+								fmt.Sprintf("\n>`@%s me` Found out how many :%s: you have",
+									info.User.Name, configuration.Emoji),
+							//								fmt.Sprintf("\n>`@%s ladder` Found out who has the most :%s:",
+							//									info.User.Name, configuration.Emoji),
+							false)
+						rtm.PostEphemeral(ev.Channel, sender.ID, text, user)
+
+					}
+				}
 
 			case *slack.RTMError:
-				fmt.Printf("Error: %s\n", ev.Error())
+				log.Printf("Error: %s\n", ev.Error())
 
 			case *slack.InvalidAuthEvent:
-				fmt.Printf("Invalid credentials")
+				log.Printf("Invalid credentials")
 				break Loop
 
 			default:
